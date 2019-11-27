0cf349fd2e74199ebf1ff752d4c35dc175bfd141
jingyu9575 <qw2g64@outlook.com>
2019-11-27 16:53:18 +0100

    Support RFC2047 in legacy filename. Close #97

--- src/background/content-disposition.ts
+++ src/background/content-disposition.ts
@@ -39,7 +39,40 @@ function decodeISO8859_1(seq: string) {
 	return new TextDecoder(encoding).decode(Uint8Array.from(arr))
 }
 
+function parseRFC2047Word(value: string) {
+	try {
+		const parts = value.split('?', 6)
+		if (parts.length !== 5 || parts[0] !== '=' || parts[4] !== '=') return undefined
+		if (!/^[-\w]+$/.test(parts[1]) || !/[!->@-~]*/.test(parts[3])) return undefined
+
+		let arr: number[]
+		if (parts[2] === 'b' || parts[2] === 'B') {
+			arr = [...atob(parts[3])].map(v => v.charCodeAt(0))
+		} else if (parts[2] === 'q' || parts[2] === 'Q') {
+			arr = (parts[2].match(/=[0-9a-fA-F]{2}|./g) || [])
+				.map(v => v.length === 3 ? parseInt(v.slice(1), 16) :
+					v === '_' ? 0x20 : v.charCodeAt(0))
+		} else return undefined
+		return new TextDecoder(parts[1]).decode(Uint8Array.from(arr))
+	} catch { return undefined }
+}
+
+function parseRFC2047(value: string) {
+	let result = ''
+	for (const s of value.split(/\s+/)) {
+		if (!s) continue
+		const part = parseRFC2047Word(s)
+		if (part === undefined) return undefined
+		result += part
+	}
+	return result
+}
+
 function parseLegacyFilename(value: string) {
+	{
+		const result = parseRFC2047(value)
+		if (result !== undefined) return result
+	}
 	if (S.legacyFilenameDetectUTF8) try {
 		return decodeURIComponent(escape(value))
 	} catch { }
